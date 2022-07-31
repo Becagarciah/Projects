a6271e2c697b80a4bba65e51906272872c76c56b
Becagarciah <104578318+Becagarciah@users.noreply.github.com>
2022-07-30 23:15:38 -0300

    Add files via upload
    ib: Asish George Tech

---
+++ sound board/script.js
@@ -0,0 +1,22 @@
+const sounds = ['risada', 'porta', 'sino', 'ventania', 'suspense'];
+
+sounds.forEach(sound => {
+   const btn = document.createElement('button');
+   btn.classList.add('btn');
+   btn.innerText = sound;
+
+   btn.addEventListener('click', () => {
+    stopPlaying();
+    document.getElementById(sound).play();
+   })
+
+   document.getElementById('buttons').append(btn);
+})
+
+function stopPlaying() {
+    sounds.forEach(sound => {
+        const audio = document.getElementById(sound);
+        audio.pause();
+        audio.currentTime = 0;
+    })
+}
