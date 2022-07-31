73689c1e800efbdfb9e4136ce56ac05e4c05b79f
Becagarciah <104578318+Becagarciah@users.noreply.github.com>
2022-07-30 22:24:36 -0300

    Add files via upload
    ib:web trend

---
+++ Form Wave/script.js
@@ -0,0 +1,7 @@
+const labels = document.querySelectorAll(".form_control label")
+labels.forEach(label =>{
+    label.innerHTML = label.innerText
+    .split("")
+    .map((letter,idx) => `<span style="transition-delay: ${idx * 300}ms">${letter}</span>`)
+    .join("")
+})
